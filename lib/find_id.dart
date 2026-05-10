22e0d3482f0271a4af20927dc62f718566e1f2e0
kksenniezz <kksenniezz@gmail.com>
2026-05-10 11:03:54 +0900

    전화번호 칸 아래 메시지 발생 시 인증번호 칸 위치 변경

--- lib/find_id.dart
+++ lib/find_id.dart
@@ -207,18 +207,19 @@ class _FindIdState extends State<FindId> {
         ),
 
         // 안내 메시지
-        Padding(
-          padding: const EdgeInsets.only(top: 8, left: 4),
-          child: Text(
-            _authStatusMessage,
-            style: TextStyle(
-              color: (_isTimeOut || _authStatusMessage == "전화번호를 확인해 주세요")
-                  ? Colors.red
-                  : const Color(0xFF235E26),
-              fontSize: 12,
+        if (_authStatusMessage.isNotEmpty)
+          Padding(
+            padding: const EdgeInsets.only(top: 8, left: 4),
+            child: Text(
+              _authStatusMessage,
+              style: TextStyle(
+                color: (_isTimeOut || _authStatusMessage == "전화번호를 확인해 주세요")
+                    ? Colors.red
+                    : const Color(0xFF235E26),
+                fontSize: 12,
+              ),
             ),
           ),
-        ),
 
         const SizedBox(height: 16),
 
