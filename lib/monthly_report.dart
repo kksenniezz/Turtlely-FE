c29ecadbe967c35fe8eef101d1066cf953369d3a
kksenniezz <kksenniezz@gmail.com>
2026-05-28 14:31:20 +0900

    '님' 뺀 버전 -> 매핑 후 다시 추가

--- lib/monthly_report.dart
+++ lib/monthly_report.dart
@@ -332,7 +332,7 @@ class _MonthlyReportViewState extends State<MonthlyReportView> {
               color: Colors.black,
             ),
             children: [
-              TextSpan(text: "$userNickname님의 "),
+              TextSpan(text: "$userNickname의 "), // 추후 '님' 추가
               TextSpan(
                 text: "월간 거북목 측정",
                 style: const TextStyle(
@@ -410,7 +410,7 @@ class _MonthlyReportViewState extends State<MonthlyReportView> {
       child: Column(
         children: [
           Text(
-            "$nickname님의 $selectedMonth 거북목 유형은...",
+            "$nickname의 $selectedMonth 거북목 유형은...", // 추후 '님' 추가
             style: TText.body.copyWith(fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 16),
