1b6f4aa1091a362d68be32e1e028888cf1d2c4b4
kksenniezz <kksenniezz@gmail.com>
2026-06-30 16:49:38 +0900

    yuv420 -> nv21 카메라 포맷

--- lib/services/mediapipe_service.dart
+++ lib/services/mediapipe_service.dart
@@ -72,7 +72,7 @@ class MediaPipeService {
         ResolutionPreset.low,
         enableAudio: false,
         imageFormatGroup: Platform.isAndroid
-            ? ImageFormatGroup.yuv420
+            ? ImageFormatGroup.nv21
             : ImageFormatGroup.bgra8888,
       );
 
@@ -306,7 +306,7 @@ class MediaPipeService {
           InputImageRotation.rotation0deg;
 
       if (Platform.isAndroid) {
-        final imageFormat = InputImageFormat.yuv420;
+        final imageFormat = InputImageFormat.nv21;
 
         final metadata = InputImageMetadata(
           size: Size(image.width.toDouble(), image.height.toDouble()),
